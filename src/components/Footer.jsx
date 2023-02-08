0c356af00c6f35606f18f11844d40b6fda42b5fe
Deep4270 <90198776+Deep4270@users.noreply.github.com>
2023-02-09 00:57:38 +0530

    successful added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>DEEPAK</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/imdeep012" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Deep4270" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
